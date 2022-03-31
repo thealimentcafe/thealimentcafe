d0ff1b32d9cc097ab919ddcc244b21e0a37492ea
subhankar <subhankar@fbc.nyc>
2022-04-01 01:18:10 +0530

    no data found

--- src/pages/orders/view.js
+++ src/pages/orders/view.js
@@ -1,6 +1,6 @@
 import React, { useEffect,useState } from 'react';
 import styles from './view.module.css';
-import { ArrowLeft, Calendar } from 'react-feather';
+import { ArrowLeft, Calendar, AlertTriangle } from 'react-feather';
 import axios from 'axios';
 import Header from "../../components/header";
 import withAuth from "../../components/withAuth";
@@ -110,6 +110,14 @@ function ViewOrders() {
                   </td>
                 </tr>)
                 })}
+                <tr>
+                  <td colSpan={8}>
+                    <div className={`${styles.NoDataFound}`}>
+                      <AlertTriangle />
+                      <p>No data Found</p>
+                    </div>
+                  </td>
+                </tr>
                 </tbody>
               </table>
             </div>
